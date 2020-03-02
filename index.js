ec8ee3c053979ac19f51f5f09452e21574384c77
Eric Buckthal <eric@simplecontacts.com>
2020-03-02 14:55:18 -0500

    debug

--- index.js
+++ index.js
@@ -4,26 +4,32 @@ const path = require('path');
 const AWS = require('aws-sdk');
 const github = require('@actions/github');
 
-try {
-  const inputBucket = core.getInput('bucket');
-  const inputPath = core.getInput('path');
-  const inputKey = core.getInput('key');
-  const key = path.join(github.context.sha, inputKey);
+async function run() {
+  try {
+    const inputBucket = core.getInput('bucket');
+    const inputPath = core.getInput('path');
+    const inputKey = core.getInput('key');
+    const key = path.join(github.context.sha, inputKey);
 
-  AWS.config.update({ region: process.env.AWS_REGION || 'us-east-1' });
-  s3 = new AWS.S3({ apiVersion: process.env.AWS_API_VERISON || '2006-03-01' });
+    AWS.config.update({ region: process.env.AWS_REGION || 'us-east-1' });
+    s3 = new AWS.S3({
+      apiVersion: process.env.AWS_API_VERISON || '2006-03-01',
+    });
 
-  await new Promise((resolve, reject) => {
-    const s3Stream = s3
-      .getObject({ Bucket: inputBucket, Key: key })
-      .createReadStream();
-    const fileStream = fs.createWriteStream(inputPath);
+    await new Promise((resolve, reject) => {
+      const s3Stream = s3
+        .getObject({ Bucket: inputBucket, Key: key })
+        .createReadStream();
+      const fileStream = fs.createWriteStream(inputPath);
 
-    s3Stream.on('error', reject);
-    fileStream.on('error', reject);
-    fileStream.on('close', () => resolve());
-    s3Stream.pipe(fileStream);
-  });
-} catch (error) {
-  core.setFailed(error.message);
+      s3Stream.on('error', reject);
+      fileStream.on('error', reject);
+      fileStream.on('close', () => resolve());
+      s3Stream.pipe(fileStream);
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
